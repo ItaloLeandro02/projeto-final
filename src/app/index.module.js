90e6dd1c13a80a36b3d68900fac6def014da6128
RiickOliveira <rcdoliveira98@hotmail.com>
2019-01-29 16:44:04 -0300

    TESTE

---
+++ src/app/index.module.js
@@ -0,0 +1,34 @@
+(function ()
+{
+    'use strict';
+
+    /**
+     * Main module of the Fuse
+     */
+    angular
+        .module('fuse', [           
+            
+            // Core
+            'app.core',
+
+            // Navigation
+            'app.navigation',
+
+            // Toolbar
+            'app.toolbar',
+
+            // Quick Panel
+            'app.quick-panel',
+
+            'app.login',
+            
+            // Sample
+            'app.sample',
+            /// Nossos modulos da aplicacao precisam ser importados
+            'app.cliente',
+
+            
+
+
+        ]);
+})();
