b2bb86ec0e8e394cc6d48f40872a8844c9605006
ItaloLeandro02 <leandroitalo433@gmail.com>
2019-02-06 18:22:15 -0300

    Construindo controller usuário

--- src/app/main/usuario/formulario/usuario.controller.js
+++ src/app/main/usuario/formulario/usuario.controller.js
@@ -15,15 +15,19 @@
         vm.tipo                                     = 'password'
         vm.status                                   = 'icon-lock-unlocked-outline'
         vm.mudarSenha                               = mudarSenha 
-        vm.permissoes                               = ['Incluir', 'Editar', 'Consultar']
+        vm.permissoes                               = ['Incluir', 'Editar', 'Excluir', 'Visualizar']
         vm.permissoesSelecionadasClientes           = []
         vm.permissoesSelecionadasUsuarios           = []
         vm.toggle                                   = toggle
         vm.exists                                   = exists
-        vm.data                                     = {}
-
 
         function salvar() {
+            vm.data.permissoes = []
+            vm.permissoesSelecionadasClientes.forEach(permissao => {
+                vm.data.permissoes.push(verificaPermissao(permissao))
+            });
+            //vm.data.permissoes.push(vm.permissoesSelecionadasClientes)
+            vm.data.permissoes.push(vm.permissoesSelecionadasUsuarios)
             console.log(vm.data.nome, vm.data.administrador, vm.data.desativado, vm.data.permissoes)
         }
 
@@ -56,6 +60,15 @@
 
         function exists(item, list) {
             return list.indexOf(item) > -1
+        }
+
+        function verificaPermissao(permissao) {
+            permissao = permissao =="Incluir" ? "01USU": permissao
+            permissao = permissao =="Editar" ? "02USU": permissao
+            permissao = permissao == "Excluir" ? "03USU": permissao
+            permissao = permissao == "Visualizar" ? "04USU": permissao
+
+            return permissao
         }
 
         vm.gridService = {
