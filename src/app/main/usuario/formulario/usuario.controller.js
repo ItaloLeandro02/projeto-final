79d9337604c50befd80068cd3d15d7531299ffa9
ItaloLeandro02 <leandroitalo433@gmail.com>
2019-02-07 09:41:49 -0300

    Construindo telas de usuário

--- src/app/main/usuario/formulario/usuario.controller.js
+++ src/app/main/usuario/formulario/usuario.controller.js
@@ -16,18 +16,27 @@
         vm.status                                   = 'icon-lock-unlocked-outline'
         vm.mudarSenha                               = mudarSenha 
         vm.permissoes                               = ['Incluir', 'Editar', 'Excluir', 'Visualizar']
+        permissoesUsuario                           = ['01USU', '02USU', '03USU', '04USU']
         vm.permissoesSelecionadasClientes           = []
         vm.permissoesSelecionadasUsuarios           = []
         vm.toggle                                   = toggle
         vm.exists                                   = exists
+        vm.data                                     = {
+            desativado      : false,
+            administrador   : false,
+            permissoes      : []
+        }
 
         function salvar() {
-            vm.data.permissoes = []
-            vm.permissoesSelecionadasClientes.forEach(permissao => {
-                vm.data.permissoes.push(verificaPermissao(permissao))
-            });
-            //vm.data.permissoes.push(vm.permissoesSelecionadasClientes)
-            vm.data.permissoes.push(vm.permissoesSelecionadasUsuarios)
+            if (!vm.data.administrador) {
+                vm.permissoesSelecionadasUsuarios.forEach(permissao => {
+                    vm.data.permissoes.push(extraiPermissaoUsuarios(permissao))
+                });
+                vm.permissoesSelecionadasClientes.forEach(permissao => {
+                    vm.data.permissoes.push(extraiPermissaoClientes(permissao))
+                });
+            }
+                          
             console.log(vm.data.nome, vm.data.administrador, vm.data.desativado, vm.data.permissoes)
         }
 
@@ -61,8 +70,17 @@
         function exists(item, list) {
             return list.indexOf(item) > -1
         }
+/*
+        function extraiData(permissao, arrayPermisoes) {
+            permissao = permissao =="Incluir" ? "01USU": permissao
+            permissao = permissao =="Editar" ? "02USU": permissao
+            permissao = permissao == "Excluir" ? "03USU": permissao
+            permissao = permissao == "Visualizar" ? "04USU": permissao
 
-        function verificaPermissao(permissao) {
+            return permissao
+        }
+*/
+        function extraiPermissaoUsuarios(permissao) {
             permissao = permissao =="Incluir" ? "01USU": permissao
             permissao = permissao =="Editar" ? "02USU": permissao
             permissao = permissao == "Excluir" ? "03USU": permissao
@@ -71,21 +89,33 @@
             return permissao
         }
 
-        vm.gridService = {
-            query : {
-                order: 'nome',
-                limit: 5,
-                page: 1
-            },
-            selected : [],
+        function extraiPermissaoClientes(permissao) {
+            permissao = permissao =="Incluir" ? "01CLI": permissao
+            permissao = permissao =="Editar" ? "02CLI": permissao
+            permissao = permissao == "Excluir" ? "03CLI": permissao
+            permissao = permissao == "Visualizar" ? "04CLI": permissao
 
-            loadData : function(){
-                return usuarioService.getDataMockup().then(function(records){
-                    console.log(records)
-                    vm.data = records
-                })   
-            }
+            return permissao
         }
+
+        function retornarPermissaoCliente(permissao) {
+            permissao = permissao == "01CLI" ? "Incluir": permissao
+            permissao = permissao == "02CLI" ? "Editar": permissao
+            permissao = permissao == "03CLI" ? "Excluir": permissao
+            permissao = permissao == "04CLI" ? "Visualizar": permissao
+
+            return permissao
+        }
+        
+        function retornarPermissaoUsuario(permissao) {
+            permissao = permissao == "01USU" ? "Incluir": permissao
+            permissao = permissao == "02USU" ? "Editar": permissao
+            permissao = permissao == "03USU" ? "Excluir": permissao
+            permissao = permissao == "04USU" ? "Visualizar": permissao
+
+            return permissao
+        }
+        
 
         function init(){
             console.log(usuarioId)
@@ -94,7 +124,18 @@
                     vm.data = records
                 })
             }*/
-            vm.gridService.loadData()
+            if (usuarioId) {
+                vm.data = {
+                    desativado      : true,
+                    administrador   : true,
+                    permissoes      : ['01USU', '02USU', '04CLI', '02CLI']
+                }
+
+                vm.data.permissoes.forEach(permissao => {
+                    vm.permissoesSelecionadasClientes.push(retornarPermissaoCliente(permissao))
+                    vm.permissoesSelecionadasUsuarios.push(retornarPermissaoUsuario(permissao))
+                })
+            }
         }
         init()
         
