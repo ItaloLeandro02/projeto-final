322ea825e5b8d2ee854bb03e2dad92851a597c08
ItaloLeandro02 <leandroitalo433@gmail.com>
2019-02-11 14:21:39 -0300

    Construindo módulo Usuário

--- src/app/main/cliente/lista/lista.controller.js
+++ src/app/main/cliente/lista/lista.controller.js
@@ -7,14 +7,15 @@
         .controller('ListaClienteController', ListaClienteController);
 
     /** @ngInject */
-    function ListaClienteController(clienteService,$state,$mdSidenav)
+    function ListaClienteController(clienteService,$state,$mdSidenav, $mdDialog)
     {
         var vm = this;
 
-        vm.novoCliente = novoCliente;
-        vm.editar = editar;
-        vm.view = view;
-        vm.logPagination = logPagination;
+        vm.novoCliente      = novoCliente;
+        vm.editar           = editar;
+        vm.view             = view;
+        vm.logPagination    = logPagination;
+        vm.excluir          = excluir;
 
         vm.gridService = {
             query : {
@@ -45,6 +46,35 @@
                 vm.data         = records.data.rows;
                 vm.data.count   = records.data.count;
             })
+        }
+
+        function excluir(ev,cliente){
+            let confirmacao = $mdDialog.confirm()
+                  .title('A	guardando confirmação')
+                  .textContent('Confirma a exclusão do cliente ' + cliente.nome)
+                  .ariaLabel('Msg interna do botao')
+                  .targetEvent(ev)
+                  .ok('Sim')
+                  .cancel('Não');
+            $mdDialog.show(confirmacao).then(function() {
+                excluirCliente(cliente.id)
+            });
+        }        
+    
+        function excluirCliente(clienteId){
+            let sucesso = function(resposta){
+                if (resposta.success) {
+                    toastr.success(resposta.message)
+                }
+                $state.go('app.cliente')
+            }
+            let erro = function(resposta) {
+                error.data.errors.forEach(erro => {
+                    toastr.error(erro)
+                })
+            }
+
+            clienteService.delete(clienteId).then(sucesso,erro)
         }
 
         function init(){
